fd67b004318f393395652d6397e41f13f092a1f6
JakovlevMA <lolipopp345@gmail.com>
2023-06-02 10:55:30 +0300

    commit three

---
+++ LearnTSAdvanced/src/namespaces.ts
@@ -0,0 +1,17 @@
+/// <reference path="form-namespace.ts/">
+namespace IForm {
+class MyForm {
+    private type: FormType = 'inline'
+    private state: FormState = 'active'
+    constructor(public email: string) {
+    }
+    getInfo(): FormInfo {
+        return {
+            type: this.type,
+            state: this.state
+        }
+    }
+}
+
+const myForm = new MyForm('hi@mail.ru')
+}
